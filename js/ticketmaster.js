72465502fcc00c78faff5c5a2858322b2827c930
Phil-Slater <pdsltr@gmail.com>
2022-01-17 22:26:54 -0500

    added apiKey and baseUrl variables and if statement for response.status

--- js/ticketmaster.js
+++ js/ticketmaster.js
@@ -1,10 +1,17 @@
 
+const apikey = 'apikey=UexS3u89soxTBHwmbNoYUHdncWFGGI2t'
+const baseUrl = 'https://app.ticketmaster.com'
+
 // by city
 export async function getEvents() {
     try {
-        url = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=${searchBox.value}&sort=date,asc&apikey=UexS3u89soxTBHwmbNoYUHdncWFGGI2t`
+        url = `${baseUrl}/discovery/v2/events.json?classificationName=music&city=${searchBox.value}&sort=date,asc&${apikey}`
         const response = await fetch(url)
-        return await response.json()
+        if (response.status === 200) {
+            return await response.json()
+        } else {
+            throw new Error('error getting data')
+        }
     } catch (error) {
         console.log(error)
     }
@@ -14,9 +21,13 @@ export async function getEvents() {
 // gives us a list of venues that are having concerts, but no list of events
 export async function getVenue() {
     try {
-        url = `https://app.ticketmaster.com/discovery/v2/venues.json?classificationName=music&apikey=UexS3u89soxTBHwmbNoYUHdncWFGGI2t`
+        url = `${baseUrl}/discovery/v2/venues.json?classificationName=music&${apikey}`
         const response = await fetch(url)
-        return await response.json()
+        if (response.status === 200) {
+            return await response.json()
+        } else {
+            throw new Error('error getting data')
+        }
     } catch (error) {
         console.log(error)
     }
@@ -26,9 +37,13 @@ export async function getVenue() {
 // returns list of events from keyword search (venue as a keyword works)
 export async function getVenue() {
     try {
-        url = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${searchBox.value}&classificationName=music&sort=date,asc&apikey=UexS3u89soxTBHwmbNoYUHdncWFGGI2t`
+        url = `${baseUrl}/discovery/v2/events.json?keyword=${searchBox.value}&classificationName=music&sort=date,asc&${apikey}`
         const response = await fetch(url)
-        return await response.json()
+        if (response.status === 200) {
+            return await response.json()
+        } else {
+            throw new Error('error getting data')
+        }
     } catch (error) {
         console.log(error)
     }
@@ -43,6 +58,3 @@ export async function getVenue() {
 //         infoDiv.insertAdjacentHTML('beforeend', `<h3>${item.name}</h3><p>${item.classifications[0].genre.name}</p><p>Date: ${item.dates.start.localDate}. Time: ${item.dates.start.localTime}</p><a href="${item.url}">Link</a><p>${item._embedded.venues[0].name}</p><p>${item._embedded.venues[0].city.name}, ${item._embedded.venues[0].state.stateCode}</p><img src="${item._embedded.attractions[0].images[1].url}"/>`)
 //     })
 // }
-
-
-https://app.ticketmaster.com/discovery/v2/events.json?keyword=statefarmarena&classificationName=music&sort=date,asc&apikey=UexS3u89soxTBHwmbNoYUHdncWFGGI2t
