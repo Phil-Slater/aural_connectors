a0bd22033c38d8b141cb2dfb6674623765e7776e
jon-cundiff <jon.cundiff@gmail.com>
2022-01-19 14:02:00 -0600

    applies dynamic footer update

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+const footer = document.getElementById("footer");
+const adjustFooterPlacement = () => {
+    if (window.innerHeight < document.body.clientHeight - 68) {
+        footer.style.position = "relative";
+    } else {
+        footer.style.postion = "absolute";
+    }
+};
+
+export const loadFooterEvents = () => {
+    adjustFooterPlacement();
+    window.addEventListener("resize", adjustFooterPlacement);
+    const observer = new MutationObserver(adjustFooterPlacement);
+    observer.observe(document.body, { childList: true, subtree: true });
+};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import TemplateImporter from "./templateImporter.js";
 import { loadHeaderData } from "./header.js";
+import { loadFooterEvents } from "./footer.js";
 
 const header = new TemplateImporter("header");
 
@@ -13,3 +14,4 @@ const footer = new TemplateImporter("footer");
 footer
     .loadHTML()
     .then(async () => footer.importElement())
+    .then(() => loadFooterEvents());
