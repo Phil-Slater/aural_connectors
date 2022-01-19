e6854030f6615e6c96dcd8834aa1f2e452d6b83d
jon-cundiff <jon.cundiff@gmail.com>
2022-01-19 17:42:01 -0600

    corrects location name display

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,5 @@
 const footer = document.getElementById("footer");
 const adjustFooterPlacement = () => {
-    console.log(window.innerHeight, "-", document.body.clientHeight - 10);
     if (window.innerHeight < document.body.clientHeight + 10) {
         footer.className = "footer-float";
     } else {
--- js/searchResults.js
+++ js/searchResults.js
@@ -1,4 +1,5 @@
 import { getConcerts, getVenues, getVenueDetails } from "./ticketmaster.js";
+import { displayConcerts } from "./displayConcerts.js";
 import { getParams } from "./getParams.js";
 
 const searchDiv = document.getElementById("search");
@@ -8,37 +9,6 @@ const params = getParams(requiredKeys);
 if (!params.searchTerm || !params.searchType) {
     window.location.assign("/");
 }
-
-const displayConcerts = (concerts) => {
-    const concertItems = concerts.map((concert) => {
-        console.log(concert);
-        const venue = concert._embedded.venues[0];
-        let venueName = venue.name;
-        if (!venueName) {
-            venueName = "";
-            getVenueDetails(venue.id).then((venueDetails) => {
-                console.log(venueDetails);
-                // searchDiv.querySelector(`.${venue.id}`).innerHTML = venueDetails;
-            });
-        }
-        const startDate = new Date(
-            concert.dates.start.dateTime
-        ).toLocaleString();
-        return `
-        <a class="concert" href="/concert.html?id=${concert.id}">
-            <div class="concertImgContainer">
-                <img class="concertImg" src=${concert.images[0].url} />
-            </div>
-            <h3>${concert.name}</h3>
-            <p><i>${venueName}</i> <b>${venue.city.name}, ${venue.country.name}</b></p>
-            <h4>${startDate}</h4>
-            <div class="hover"></div>
-        </a>
-    `;
-    });
-
-    searchDiv.innerHTML = concertItems.join("");
-};
 
 const displayVenues = (venues) => {
     console.log(venues);
